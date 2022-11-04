24dcc0c65d58367cf4859f2a1e85a8e3383e41af
Steven <bongamke@sheridancollege.ca>
2022-11-04 17:11:39 -0400

    postcss.config file created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import styles from './style'
 
 const App = () => (
-  <div className='bg-primary w-full overflow-hidden'>
-    <h1>Hello, Wo</h1>
+  <div className="bg-primary w-full overflow-hidden">
+    <h1>Hello, Weo</h1>
   </div>
 )
 
