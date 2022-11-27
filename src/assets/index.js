5504e5fc6fc030b4423399dc82a80dd6e5d52bb3
Steven <bongamke@sheridancollege.ca>
2022-11-27 15:13:13 -0500

    Hero section updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg'
 import menu from './menu.svg'
 import close from './close.svg'
+import avatar from './avatar.svg'
 
 export {
     logo,
     menu,
     close,
+    avatar,
 }
