79fe6207ab77ef9de32718fe494db6a3cc7fe953
Steven <bongamke@sheridancollege.ca>
2023-01-11 13:20:43 -0500

    project section edited

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,8 @@ import styles, { layout } from "../style"
 import LearnMoreBtn from "./LearnMoreBtn"
 
 const ProjectCard = ({ title, category, description, link, stack, index }) => (
-  <div className={`flex flex-col items-center p-6 rounded-[20px] ${index !== projects.length - 1 ? "mb-6" : "md-0"} project-card`}>
-    <div className="border border-[#262626] rounded-[20px] h-[200px] w-[100%] sm:h-[296px] sm:w-[100%] mb-5"><img /></div>
+  <div className={`flex flex-col items-center p-6 rounded-[20px] ${index !== projects.length - 1 ? "mb-6" : "md-0"}`}>
+    <div className="border-2 border-[#262626] rounded-[20px] h-[200px] w-[100%] sm:h-[296px] sm:w-[100%] mb-5"><img /></div>
     <div className="flex-1 flex flex-col ml-3 px-2">
       <h4 className="font-poppins font-semibold text-white text-[1em] leading-[23px] mb-1">{title}</h4>
       <p className="font-poppins text-[#999999] text-[0.75em] leading-[20px] mb-1">{description}</p>
