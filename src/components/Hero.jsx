93e8ac895efb7d01c52b5599e39ecb2f25e42e6b
Steven <bongamke@sheridancollege.ca>
2023-01-12 00:43:50 -0500

    Footer Component Finalized

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () =>  (
           initial='initial'
           animate='animate'
           className="flex-1 font-poppins mb-2 font-semibold text-center ss:text-[4em] text-[2em] text-white ss:leading-[80px] leading-[20px]">
-          Hello, I'm <span className=" text-[#6E07F3]">Steven.</span>
+          Hello, I'm <span className=" text-[#303798]">Steven.</span>
         </motion.h1>
 
         <motion.p
@@ -28,7 +28,7 @@ const Hero = () =>  (
           variants={headerCTAVariant}
           initial='initial'
           animate='animate'
-          className="bg-[#6E07F3] font-poppins cursor-pointer text-[16px] font-semibold py-4 px-10 rounded-full inline-flex items-center text-white">
+          className="bg-[#303798] font-poppins cursor-pointer text-[16px] font-semibold py-4 px-10 rounded-full inline-flex items-center text-white">
           View My Work
         </motion.a>
       </div>
