8313aa883f7a8333a15ffdbd4119d3d93ce8caee
Steven <bongamke@sheridancollege.ca>
2023-01-14 19:25:49 -0500

    nav issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Navbar, Hero, Projects, Footer } from './components'
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    
+    <div className='fixed top-0 w-full h-[100px] z-10 bg-primary '></div>
+
     <div className={`${styles.paddingX} ${styles.flexCenter} fixed w-full z-10 top-0`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
